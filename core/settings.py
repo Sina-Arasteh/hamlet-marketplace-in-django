321c5395f332ed2b09e7a689ef11e61bb1f0c078
Sina Arasteh <sinna.arasteh@gmail.com>
2025-04-03 17:45:41 +0330

    Change the values of the some of the variables

--- core/settings.py
+++ core/settings.py
@@ -103,11 +103,11 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fr-ir'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'IR'
 
-USE_I18N = True
+USE_I18N = False
 
 USE_TZ = True
 
@@ -121,3 +121,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+FIRST_DAY_OF_WEEK = 6
+
+NUMBER_GROUPING = 3
+USE_THOUSAND_SEPARATOR = True
+
+DECIMAL_SEPARATOR = '/'
